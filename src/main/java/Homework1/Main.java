3174d2ad84a3d7cbe7438e936861455d4f715484
Maxim <maksfomin@gmail.com>
2020-02-04 14:06:32 +0300

    Create HW 1 step 2

--- src/main/java/Homework1/Main.java
+++ src/main/java/Homework1/Main.java
@@ -7,7 +7,8 @@ public class Main {
     public static void main(String[] args) {
 
         int[] nums = {-7, -6, -5, -4, -3, -2, -1, 0, 1, 2, 3, 4, 5, 6, 7};
-        System.out.println((sum(nums)));
+        System.out.println(sum(nums) + "\n");
+        System.out.println(num(nums));
     }
 
     private static ArrayList<String> sum (int[] numbers) {
@@ -28,5 +29,20 @@ public class Main {
             }
         }
         return allNums;
+    }
+
+    private static ArrayList<Integer> num (int[] numbers) {
+        ArrayList<Integer> num = new ArrayList<>();
+        int tmp = 0;
+        int tmp1 = 1;
+        for (int i : numbers) {
+           if (i > tmp1) {
+               tmp = tmp1;
+               tmp1 = i;
+           }
+        }
+        num.add(tmp);
+        num.add(tmp1);
+        return num;
     }
 }
